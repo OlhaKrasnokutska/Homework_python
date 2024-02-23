707b31496eedc629bdb01d7caa06bb600f34c791
Olha Krasnokutska <olechka310588@gmail.com>
2024-02-23 16:31:20 +0100

    Created add/subtract

---
+++ app.py
@@ -0,0 +1,22 @@
+number1 = int(input("Enter first number: "))
+number2 = int(input("Enter second number: "))
+operator_ = input('Enter operator "+" or "-": ')
+
+
+def add(num1, num2):
+    print(num1 + num2)
+
+def subt(num1, num2):
+    print(num1 - num2)
+
+
+def calc(num1, num2, operator):
+    if operator == "+":
+        add(num1, num2)
+    elif operator == "-":
+        subt(num1, num2)
+    else:
+        print("Choose correct operator")
+
+
+calc(number1, number2, operator_)
