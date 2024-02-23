2df601d3b09df7b93cb3f0df290bb05ffdd1ef5d
Olha Krasnokutska <olechka310588@gmail.com>
2024-02-23 16:44:16 +0100

    Added division

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 number1 = int(input("Enter first number: "))
 number2 = int(input("Enter second number: "))
-operator_ = input('Enter operator "+" or "-": ')
+operator_ = input('Enter operator "+", "-" or "/": ')
 
 
 def add(num1, num2):
@@ -9,12 +9,16 @@ def add(num1, num2):
 def subt(num1, num2):
     print(num1 - num2)
 
+def div(num1, num2):
+    print(num1 / num2)
 
 def calc(num1, num2, operator):
     if operator == "+":
         add(num1, num2)
     elif operator == "-":
         subt(num1, num2)
+    elif operator == "/":
+        div(num1, num2)
     else:
         print("Choose correct operator")
 
